ab72d0b3775034b5185a23e4855cc88c4354ca0d
philonoun <philonoun@protonmail.com>
2019-08-02 23:44:11 -0500

    new updates

---
+++ client/src/components/Ping.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template
+
+<script>
+export default {
+  name: 'Ping',
+  date() {
+    dreturn {
+      msg: 'Hello',
+    };
+  };
+};
+</script>
